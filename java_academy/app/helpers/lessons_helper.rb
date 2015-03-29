1c3a0b4917068f181f527c794bf64a0350b258da
Dang Minh Nguyen <ndminh92@gmail.com>
2015-03-29 19:22:05 -0400

    Added submit functions in app/helpers/lessons_helper.rb
    Allow for submission of code to Hackerrank API

--- java_academy/app/helpers/lessons_helper.rb
+++ java_academy/app/helpers/lessons_helper.rb
@@ -1,2 +1,24 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 module LessonsHelper
+    # Submit lesson via hackerrank API
+    # See https://www.hackerrank.com/api/docs for details
+
+    HACKERRANK_API_KEY = "hackerrank|204549-190|a37e68ec781f2525a479a5971c43772cb13b0013"
+
+    def submit_work(code, testcases)
+        uri = URI("http://api.hackerrank.com/checker/submission.json")
+        form = {"source" => code, "lang" => 5, "testcases" => testcases.to_json,
+                "api_key" => "hackerrank|204549-190|a37e68ec781f2525a479a5971c43772cb13b0013",
+                'format' => 'json'}
+        
+        response = Net::HTTP.post_form(uri, form)
+
+    end
+
+    def test_submit
+        submit_work('print 1', ["1"])
+    end
 end
