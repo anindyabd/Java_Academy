8270644673423a402bf4b5ec22789df7113ef690
Dang Minh Nguyen <ndminh92@gmail.com>
2015-03-31 19:39:50 -0400

    Clean up testing codes in the submit function

--- java_academy/app/helpers/lessons_helper.rb
+++ java_academy/app/helpers/lessons_helper.rb
@@ -3,19 +3,26 @@ require 'uri'
 require 'json'
 
 module LessonsHelper
-    # Submit lesson via hackerrank API
-    # See https://www.hackerrank.com/api/docs for details
 
     HACKERRANK_API_KEY = "hackerrank|204549-190|a37e68ec781f2525a479a5971c43772cb13b0013"
 
     def submit_work(code, testcases)
+        # Submit lesson via hackerrank API
+        # See https://www.hackerrank.com/api/docs for details
         uri = URI("http://api.hackerrank.com/checker/submission.json")
-        form = {"source" => code, "lang" => 5, "testcases" => testcases.to_json,
+        form = {"source" => code, 
+                "lang" => 3, # Number code for Java
+                "testcases" => testcases.to_json,
                 "api_key" => "hackerrank|204549-190|a37e68ec781f2525a479a5971c43772cb13b0013",
                 'format' => 'json'}
         
         response = Net::HTTP.post_form(uri, form)
 
+        if response.code == "200" # Valid response
+            result = JSON.parse(response.body)
+        else
+            result = nil
+        end
     end
 
     def test_submit
