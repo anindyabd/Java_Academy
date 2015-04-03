81429a78810f10fe5642a3ae83b3dc5a45c7c1a2
Anindya <anindyabd@gmail.com>
2015-04-03 16:44:39 -0400

    Submits now routed to new page and works as it should (no longer hard coded). Created index page.

--- java_academy/app/controllers/lessons_controller.rb
+++ java_academy/app/controllers/lessons_controller.rb
@@ -1,15 +1,17 @@
 class LessonsController < ApplicationController
 
     def index
-        # This is mostly for testing if this idea works. It is hard coded
-        if params[:realacesubmit] != nil and params[:realacesubmit] != ""
-            resulting_code = Lesson.find(2).check_submission(params[:realacesubmit]) 
-            @result = resulting_code
-        end
+       @lessons = Lesson.all  
     end 
 
     def show
         @lesson = Lesson.find(params[:id])
     end 
 
+    def submit 
+        if params[:realacesubmit] != nil and params[:realacesubmit] != ""
+            resulting_code = Lesson.find(params[:lessonid]).check_submission(params[:realacesubmit]) 
+            @result = resulting_code
+        end
+    end 
 end
--- java_academy/config/routes.rb
+++ java_academy/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+    
+    root 'lessons#index' 
     resources :lessons
     resources :users
+
+    post 'lessons/submit', to: 'lessons#submit' 
 end
