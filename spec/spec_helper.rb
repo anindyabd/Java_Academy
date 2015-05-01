a13086a8907defd2ef4202408bb53eb384cf6a9f
Dang Minh Nguyen <ndminh92@gmail.com>
2015-05-01 00:52:46 -0400

    Added databaseCleaner config to spec
    Previously spec modify databse and do not clean up
    It now do, and won't intefere with cucumber testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -86,7 +86,7 @@ RSpec.configure do |config|
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
+  # the seed, which is printed Rfter each run.
   #     --seed 1234
   config.order = :random
 
@@ -96,4 +96,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  config.after(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
 end
